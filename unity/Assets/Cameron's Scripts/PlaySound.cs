1b819ae0e6f378b74345f981cdc6a1b2cf664e90
Cameron Bennett <cameronbennett@Camerons-MacBook-Air.local>
2018-11-03 03:44:57 -0500

    Audio Beta

---
+++ unity/Assets/Cameron's Scripts/PlaySound.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class PlaySound : MonoBehaviour {
+    public AudioSource audio;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+        if (Input.GetMouseButton(0))
+        {
+            audio.enabled = true;
+            audio.loop = true;
+            Debug.Log(TeleportalPlayer.Current.gameObject.transform.position.x);
+            audio.pitch = TeleportalPlayer.Current.gameObject.transform.position.x;
+            audio.volume = TeleportalPlayer.Current.gameObject.transform.position.z;
+
+        }
+        else
+        {
+
+            audio.enabled = false;
+            audio.loop = false;
+        }
+    }
+}
