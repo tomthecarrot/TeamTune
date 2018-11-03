32b1abd54a208097e101fe654d4a81d6ca45aa65
Matt Kelsey <mattkelseymk@gmail.com>
2018-11-03 03:09:04 -0400

    Create basic scene

---
+++ unity/Assets/ColorShifter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorShifter : MonoBehaviour {
+
+  Light lt;
+	// Use this for initialization
+	void Start () {
+		lt = GetComponent<Light>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		lt.color -= (Color.HSVToRGB(0.3f, 0.0f, 0.0f, true) / 2.0f) * Time.deltaTime;
+	}
+}
